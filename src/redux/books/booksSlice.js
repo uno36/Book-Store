a84cd60fdfcdfd1afe1971f45c1f1b79984687a5
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-06-19 23:12:25 +0200

    add redux toolkit

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      const { title, author, category } = action.payload;
+      const newBook = {
+        id: state.books.length + 1,
+        title,
+        author,
+        category,
+      };
+      state.books.push(newBook);
+    },
+    removeBook: (state, action) => {
+      const bookId = action.payload;
+      state.books = state.books.filter((book) => book.id !== bookId);
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+export default bookSlice.reducer;
---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  status: 'Under construction',
+};
+
+const categorySlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: { },
+});
+
+export default categorySlice.reducer;
